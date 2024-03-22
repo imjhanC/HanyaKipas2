8fa37af6f161e954bda0251fdf40f08b2ce20ab7
TanKyeWen <tkyewen@gmail.com>
2024-03-22 13:44:30 +0800

    commit

--- Includes/navigationBar.php
+++ Includes/navigationBar.php
@@ -6,7 +6,7 @@
         <ul class="nav-links">
             <input type="text" placeholder="Search">
             <a href="#xxx"><img src="shopping-cart.png" alt="shopping cart" height =50 width =50></a></img>
-            <a href="Login/index.php"><img src="login.png" alt="shopping cart" height =50 width =50 id="loginlogo"></a></img>
+            <a href="../Login/index.php"><img src="login.png" alt="shopping cart" height =50 width =50 id="loginlogo"></a></img>
         </ul>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -58,18 +58,9 @@
         <br>
         <br>
         <br>
-        <footer class="footer">
-            <div id="privacy-info">
-                <p>Privacy Policy</p>
-                <p>Terms & Conditions</p>
-            </div>
-            <div id="companyinfo1">
-                <p>&#169 HanyaKipas Sdn. Bhd 2024</p>
-            </div>
-            <div id="companyinfo2">
-                HanyaKipas Sdn. Bhd | The best fan selling company in Malaysia (Co No.6969696969)
-            </div>
-        </footer>
+        <?php
+            include 'footer.php';
+        ?>
         <script src="script.js"></script>
     </body>
 </html>
