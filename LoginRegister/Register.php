0b566a550b8982ab2080d458130f2c6071e0f484
imjhanC <jhan9502@gmail.com>
2024-03-27 19:49:06 +0800

    register page

---
+++ LoginRegister/Register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+        <title>Register account</title>
+    </head>
+    <body>
+        <form id="registerPage" method="POST">
+            <label for="newUsername">Username</label>
+            
+
+        </form>
+    </body>
+</html>
