07e93c18fae4d82a4bd345c10e741eaea0b16687
imjhanC <jhan9502@gmail.com>
2024-03-27 15:28:55 +0800

    added the registerIndex validation form

--- LoginRegister/RegisterIndex.php
+++ LoginRegister/RegisterIndex.php
@@ -10,29 +10,33 @@
 <body>
 
     <div class="wrapper">
-        <h1>REGISTER</h1>
+        <h1>Register an Account</h1>
 
         <!-- Registration Form -->
         <form method = "post" action = "../index.html">
             <div class="input-box">
-                <input type="text" id="uUsername" placeholder="Username" reqiured>
+                <input type="text" id="uUsername" placeholder="Username" required>
                 <box-icon name='user' type='solid' ></box-icon>
             </div>
+            <div id="userName-error" style="color: red;"></div>
 
             <div class="input-box">
-                <input type="email" id="uEmail" placeholder="Email" reqiured>
+                <input type="email" id="uEmail" placeholder="Email" required>
                 <box-icon name='envelope' type='solid' ></box-icon>
             </div>
+            <div id="email-error" style="color: red;"></div>
 
             <div class="input-box">
-                <input type="password" id="uPassword" placeholder="Password" reqiured>
+                <input type="password" id="uPassword" placeholder="Password" required>
                 <box-icon name='lock-alt' type='solid' ></box-icon>
             </div>
+            <div id="password-error" style="color: red;"></div>
 
             <div class="input-box">
-                <input type="password" id="uRePassword" placeholder="Re-Enter Password" reqiured>
+                <input type="password" id="uRePassword" placeholder="Re-Enter Password" required>
                 <box-icon name='lock-alt' type='solid' ></box-icon>
             </div>
+            <div id="repassword-error" style="color: red;"></div>
 
             <button type="submit" class="btn">Register</button>
 
@@ -46,16 +50,28 @@
 
     <!-- To validate if the Password == Re-Enter Password -->
     <script>
-        function validatePasswords() {
-            let password = document.getElementById('uPassword').value;
-            let reenterPassword = document.getElementById('uRePassword').value;
-
-            if (uPassword !== uRePassword) {
-                alert('Passwords do not match.');
-                return false;
-            }
+        funnction validateForm() {
+            var userName =document.getElementById("uUsername").value;
+            var emailAddress =document.getElementById("uEmail").value;
+            var password =document.getElementById("uPassword").value;
+            var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             
-            return true;
+            var isValid  = true;
+            //Validate username
+            if(userName.trim() === ''){
+                document.getElementById("userName-error").innerText = 'Name is required';
+                isValid = false; 
+            }  else {
+                document.getElementById("userName-error").innerText = '';
+            } 
+
+            //Validate email address
+            if(emailAddress.trim() === '' || emailPattern.test(emailAddress)){
+                document.getElementById("email-error").innerText = 'Email Address is required';
+                isValid = false; 
+            } else {
+                document.getElementById("email-error").innerText = '';
+            } 
         }
     </script>
 
