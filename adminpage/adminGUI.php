5ea2ea59bd4946af5d6660017a03450fbc26e876
imjhanC <jhan9502@gmail.com>
2024-04-05 16:18:07 +0800

    admin page

--- adminpage/adminGUI.php
+++ adminpage/adminGUI.php
@@ -1,30 +1,45 @@
 <!DOCTYPE html>
-    <head>
-        <link rel="stylesheet" href="adminstyles.css">
-    </head>
-    <body>
-        <nav id="desktop-nav">
-            <div class="text-logo">
-                <img src ="logo.png" width =150px height =90px></img>
-            </div>
-            <div>
-                <ul class="nav-links">
-                    <div id ="search">
-                        <p> I love web dev</p>
-                    </div>
-                    <div id="results">
-                    
-                    </div>
-                    <a href="#xxx"><img src="shopping-cart.png" alt="shopping cart" height =50 width =50></a></img>
-                    <a href="../../HanyaKipas/LoginRegister/LoginGUI.php"><img src="login.png" alt="shopping cart" height =50 width =50 id="loginlogo"></a></img>
-                </ul>
-            </div>
-        </nav>
-        <div class="sidenav">
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
-            <a href="#clients">Clients</a>
-            <a href="#contact">Contact</a>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <title>HanyaKipas : Admin Page</title>
+    <link rel="stylesheet" href="adminstyles.css">
+</head>
+<body>
+    <nav id="desktop-nav">
+        <div class="text-logo">
+            <img src ="logo.png" width =150px height =90px></img>
         </div>
-    </body>
+        <!-- Your navigation items here -->
+    </nav>
+    <div class="sidebar">
+        <a href=#about>Home</a>
+        <a href=#about>Create product</a>
+        <a href=#about>Update product</a>
+        <a href=#about>Delete product</a>
+    </div>
+    <div class="info-home">
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum asperiores maiores excepturi ex nesciunt id ratione, adipisci quae modi laboriosam, labore placeat, suscipit magni eos exercitationem architecto quaerat hic doloribus.</p>
+        <br>
+        <br>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum asperiores maiores excepturi ex nesciunt id ratione, adipisci quae modi laboriosam, labore placeat, suscipit magni eos exercitationem architecto quaerat hic doloribus.</p>
+        <div class="search-bar">
+            <input type="text" placeholder="Search...">
+            <button>Search</button>
+        </div><div class="search-bar">
+            <input type="text" placeholder="Search...">
+            <div class="dropdown">
+                <button class="dropbtn">Dropdown</button>
+                <div class="dropdown-content">
+                    <a href="#">Option 1</a>
+                    <a href="#">Option 2</a>
+                    <a href="#">Option 3</a>
+                </div>
+            </div>
+            <button>Search</button>
+        </div>
+    <div>   
+</body>
 </html>
