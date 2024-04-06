c663e3a8980c5a872b6c6d2f5c737f80275c983f
imjhanC <chengjiahan@outlook.com>
2024-04-06 10:08:23 +0800

    contact page

--- ContactUs/ContactUs.php
+++ ContactUs/ContactUs.php
@@ -4,13 +4,97 @@
 		<meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-		<title>Contact Us</title>
+		<title>HanyaKipas: Contact Us</title>
+		<link rel="stylesheet" href="contactstyles.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
 	</head>
 	<body>
-		<nav id="desktop-nav">
+		<!-- navigation bar -->
+        <nav id="desktop-nav">
             <div class="text-logo">
                 <img src ="logo.png" width =150px height =90px></img>
             </div>
+            <div>
+                <ul class="nav-links">
+                    <div id="search">
+                        <svg viewBox="0 0 420 60" xmlns="http://www.w3.org/2000/svg">
+                            <rect class="bar"/>
+                            
+                            <g class="magnifier">
+                                <circle class="glass"/>
+                                <line class="handle" x1="32" y1="32" x2="44" y2="44"></line>
+                            </g>
+                    
+                            <g class="sparks">
+                                <circle class="spark"/>
+                                <circle class="spark"/>
+                                <circle class="spark"/>
+                            </g>
+                    
+                            <g class="burst pattern-one">
+                                <circle class="particle circle"/>
+                                <path class="particle triangle"/>
+                                <circle class="particle circle"/>
+                                <path class="particle plus"/>
+                                <rect class="particle rect"/>
+                                <path class="particle triangle"/>
+                            </g>
+                            <g class="burst pattern-two">
+                                <path class="particle plus"/>
+                                <circle class="particle circle"/>
+                                <path class="particle triangle"/>
+                                <rect class="particle rect"/>
+                                <circle class="particle circle"/>
+                                <path class="particle plus"/>
+                            </g>
+                            <g class="burst pattern-three">
+                                <circle class="particle circle"/>
+                                <rect class="particle rect"/>
+                                <path class="particle plus"/>
+                                <path class="particle triangle"/>
+                                <rect class="particle rect"/>
+                                <path class="particle plus"/>
+                            </g>
+                        </svg>
+                        <input type=search name=q aria-label="Search for inspiration"/>
+                    </div>
+                    
+                    <div id="results">
+                        
+                    </div>
+                    <a href="#xxx"><img src="shopping-cart.png" alt="shopping cart" height =50 width =50></a></img>
+                    <a href="../../HanyaKipas/LoginRegister/LoginGUI.php"><img src="login.png" alt="shopping cart" height =50 width =50 id="loginlogo"></a></img>
+                </ul>
+            </div>
         </nav>
+		<br>
+		<section id = "contact-us-form">
+			<div id = "how-can-we-help">
+				<h1>How can we help you ?</h1>
+			</div>
+		</section>
+		<br>
+		<br>
+		<footer class="footer">
+            <div id="copyright">
+                <p>Copyright &#169 HanyaKipas Sdn. Bhd 2024</p>
+                <p>HanyaKipas Sdn. Bhd || The best fan selling company in Malaysia (Co No.6969)</p>
+            </div>
+            <br>
+            <div id="footerbuttons">
+                <a id="aboutfooter">About HanyaKipas </a>
+                <a id="productfooter">Products</a>
+                <a id="privacypolicyfooter">Privacy Policy</a>
+                <a id="contactUsfooter">Contact Us</a>
+            </div>
+        </footer>
 	</body>
 </html>
