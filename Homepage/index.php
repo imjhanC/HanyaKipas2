2d670d2df38b276c64dbdfccbd6e2471144d2ac9
imjhanC <chengjiahan@outlook.com>
2024-04-13 17:15:01 +0800

    product type added

--- Homepage/index.php
+++ Homepage/index.php
@@ -74,25 +74,13 @@
                     </div>
                     <a href="#xxx"><img src="shopping-cart.png" alt="shopping cart" height =50 width =50></a></img>
                     <div class="dropdown">
-                    <?php
-session_start();
-if(!isset($_SESSION['login'])){
-    // Not logged in
-    echo '<a href="../../HanyaKipas/LoginRegister/LoginGUI.php">
-            <img src="login.png" alt="shopping cart" height="50" width="50" id="loginlogo" onclick="toggleDropdown()">
-         </a>';
-} else {
-    // Logged in
-    echo '<a>
-            <img src="login.png" alt="shopping cart" height="50" width="50" id="loginlogo" onclick="toggleDropdown()">
-         </a>
-         <div class="dropdown-content" id="dropdownContent" style="display:block;">
-            <a href="#">Account Info</a>
-            <a href="../../HanyaKipas2/LoginRegister/logout.php">Logout</a>
-         </div>';
-}
-?>
-
+                        <a href="../../HanyaKipas/LoginRegister/LoginGUI.php">
+                            <img src="login.png" alt="shopping cart" height="50" width="50" id="loginlogo" onclick="toggleDropdown()">
+                        </a>
+                        <div class="dropdown-content" id="dropdownContent" style="display:block;">
+                            <a href="#">Account Info</a>
+                            <a href="../../HanyaKipas2/LoginRegister/logout.php">Logout</a>
+                        </div>
                     </div>
                 </ul>
             </div>
