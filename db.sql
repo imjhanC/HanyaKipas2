cc73e3cb72a31c6eeebe3d00aeef6091e94b04e6
imjhanC <chengjiahan@outlook.com>
2024-04-19 03:08:49 +0800

    db changed

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE hanyakipas CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 USE hanyakipas
 
-CREATE TABLE user (
+CREATE TABLE `user` (
     `username` VARCHAR(50) NOT NULL,
     `emailaddress` VARCHAR(50) NOT NULL,
     `password` VARCHAR(50) NOT NULL,
